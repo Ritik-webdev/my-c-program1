764d76035694e8b415089f3552ac06f994ef9dcd
Ritik-webdev <singhritik9438@gmail.com>
2026-09-17 12:39:56 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,12 @@
+//WAP to find a leap year
+#include<stdio.h>
+int main(){
+	int year;
+	printf("enter year:");
+	scanf("%d", &year);
+	if (year % 4 != 0)
+		printf("leap year");
+	else 
+		printf("not a leap year");
+	return 0;
+}
