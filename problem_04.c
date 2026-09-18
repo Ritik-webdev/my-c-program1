b09454d1444314f34efcb84c7421237fcc3e9e2b
Ritik-webdev <singhritik9438@gmail.com>
2026-09-18 22:33:48 +0530

    new

---
+++ problem_04.c
@@ -0,0 +1,13 @@
+//wap to add a two number
+#include<stdio.h>
+int main(){
+	int a, b, sum;
+	printf("enter your first number:");
+	scanf("%d", &a);
+	printf("enter your second number:");
+	scanf("%d", &b);
+	sum = a + b;
+	printf("the sum of first number and second number is %d", sum);
+	return 0;
+}
+	
