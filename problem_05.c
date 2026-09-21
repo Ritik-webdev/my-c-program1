4b71d6d69ffcda90126ad7f7cde0ec02cbefa533
Ritik-webdev <singhritik9438@gmail.com>
2026-09-21 22:01:27 +0530

    new

---
+++ problem_05.c
@@ -0,0 +1,15 @@
+// Wap to swap two numbers
+#include<stdio.h>
+int main(){
+	 int first, second, temp;
+    printf("Enter first number: ");
+    scanf("%d", &first);
+    printf("Enter second number: ");
+    scanf("%d", &second);
+    printf("\nBefore swapping: First = %d, Second = %d\n", first, second);
+    temp = first;   
+    first = second; 
+    second = temp;  
+    printf("After swapping: First = %d, Second = %d\n", first, second);
+    return 0;
+}
